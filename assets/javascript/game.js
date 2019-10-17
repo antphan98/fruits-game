46030fd3f352c9f77f491881a5a206c72c75f2fe
An Phan <antphan98@gmail.com>
2019-10-16 20:22:56 -0500

    click counter for apple

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,42 @@ let counter = 0;
 
 // for (let i = 0; i > fruitNum.length; i++) {
 
-// console.log();
-//     // const fruitPicture = $("<img>");
-//     // fruitPicture.addClass("apple");
-//     // fruitPicture.attr("src", "hhttps://cdn1.iconfinder.com/data/icons/fruit-cartoon-flat-cute-fruity/512/apple-512.png");
-//     // fruitPicture.attr("data-appleValue", numberOptions[i]);
-//     // $(".apple").append(fruitPicture);
+//     const fruitPicture = $("<img>");
+//     fruitPicture.addId("apple");
+//     fruitPicture.attr("src", "https://cdn1.iconfinder.com/data/icons/fruit-cartoon-flat-cute-fruity/512/apple-512.png");
+//     fruitPicture.attr("numAppleValue", numberOptions[i]);
+//     $("#apple").append(fruitPicture);
 
 
 // }
 
-$("#apple").on("click", function() {
 
-    alert("ahh");
+    $("#apple").on("click", function() {
 
-    
+    // const appleValue = ($(this).attr("numAppleValue"));
+    // appleValue = parseInt(appleValue);
+    counter += 12;
+    alert("You clicked this crystal " + counter + " times!");
+
+// })
+
+// $("#orange").on("click", function() {
+
+//     alert("ahh");
 
 
-})
+// })
+
+// $("#grape").on("click", function() {
+
+//     alert("ahh");
+
+
+// })
+
+// $("#pear").on("click", function() {
+
+//     alert("ahh");
+
+
+ });
