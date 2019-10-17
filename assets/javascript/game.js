7cb9a54625d224b16dcdf782ad689c8dd789ab58
An Phan <antphan98@gmail.com>
2019-10-16 20:06:51 -0500

    added click event to images

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+const targetNumber = 48;
+const fruitNum = [12, 9, 4, 18];
+let counter = 0;
+
+
+// for (let i = 0; i > fruitNum.length; i++) {
+
+// console.log();
+//     // const fruitPicture = $("<img>");
+//     // fruitPicture.addClass("apple");
+//     // fruitPicture.attr("src", "hhttps://cdn1.iconfinder.com/data/icons/fruit-cartoon-flat-cute-fruity/512/apple-512.png");
+//     // fruitPicture.attr("data-appleValue", numberOptions[i]);
+//     // $(".apple").append(fruitPicture);
+
+
+// }
+
+$("#apple").on("click", function() {
+
+    alert("ahh");
+
+    
+
+
+})
